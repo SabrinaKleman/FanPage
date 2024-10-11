9ba6eea854322e2b3f14870abe73e4d368027cc7
master <sabrinakleman@gmail.com>
2024-10-11 12:34:59 -0400

    added button and styling

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import React from 'react';
 import './App.css';  // Import custom styles
 import repeatTextImg from './images/repeatText.png';
+import telegramIcon from './images/telegram.png';
+
 
 function App() {
   return (
-    <div className="App">
+   <div className="App">
       <header className="hero">
         <div className="hero-content">
-          <h1>Game Time</h1>
-          <button className="hero-button">Get Started</button>
+          {/* Layered image */}
+          <img src={repeatTextImg} alt="Repeat Text" className="centered-image" />
+<button className="hero-button">
+      <img src={telegramIcon} alt="Telegram Icon" className="button-icon" />
+      GOT GAME?
+    </button>
+    <p className="telegram-text">PLAY NOW ON TELEGRAM</p> {/* New text */}
         </div>
-        <img src={repeatTextImg} alt="Repeat Text" className="centered-image" /> {/* Add the image */}
+
       </header>
     </div>
   );
 }
-
 export default App;
