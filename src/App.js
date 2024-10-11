cd210d7e81c73575b47201366f28fab0dd4e2ead
master <sabrinakleman@gmail.com>
2024-10-11 11:44:24 -0400

    game time image

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import './App.css';  // Import custom styles
+import repeatTextImg from './images/repeatText.png';
 
 function App() {
   return (
     <div className="App">
       <header className="hero">
         <div className="hero-content">
-          <h1>Welcome to My Landing Page</h1>
+          <h1>Game Time</h1>
           <button className="hero-button">Get Started</button>
         </div>
+        <img src={repeatTextImg} alt="Repeat Text" className="centered-image" /> {/* Add the image */}
       </header>
     </div>
   );
