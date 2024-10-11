84a2cb5bd9f40ad5c22a3b7748a19bfb8304bc4b
master <sabrinakleman@gmail.com>
2024-10-11 13:53:50 -0400

    animations and other stuff

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import React from 'react';
 import './App.css';  // Import custom styles
-import repeatTextImg from './images/repeatText.png';
+import repeatTextImg from './images/repeatText.png'; // Adjust your import paths
 import telegramIcon from './images/telegram.png';
-
+import iphoneImage from './images/phone.png'; // Ensure this matches the correct file name
 
 function App() {
   return (
-   <div className="App">
+    <div className="App">
       <header className="hero">
         <div className="hero-content">
-          {/* Layered image */}
+          {/* Centered image */}
           <img src={repeatTextImg} alt="Repeat Text" className="centered-image" />
-<button className="hero-button">
-      <img src={telegramIcon} alt="Telegram Icon" className="button-icon" />
-      GOT GAME?
-    </button>
-    <p className="telegram-text">PLAY NOW ON TELEGRAM</p> {/* New text */}
-        </div>
 
+          {/* Button with image */}
+          <button className="hero-button">
+            <img src={telegramIcon} alt="Telegram Icon" className="button-icon" />
+            GOT GAME?
+          </button>
+          
+          {/* Text below the button */}
+          <p className="telegram-text">PLAY NOW ON TELEGRAM</p>
+        </div>
       </header>
+      
+      {/* Bottom iPhone image */}
+      <div className="bottom-image-container">
+        <img src={iphoneImage} alt="iPhone" className="bottom-image" />
+      </div>
     </div>
   );
 }
+
 export default App;
