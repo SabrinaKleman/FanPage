d07eca94da1c89b5efb44d0728c111044f2c5a25
master <sabrinakleman@gmail.com>
2024-10-11 22:42:39 -0400

    all the changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import './App.css';  // Import custom styles
-import repeatTextImg from './images/repeatText.png'; // Adjust your import paths
+import game1 from './images/Game1.png';
+import game2 from './images/Game2.png';
+import game3 from './images/Game3.png';
+import game4 from './images/Game4.png';
 import telegramIcon from './images/telegram.png';
-import iphoneImage from './images/phone.png'; // Ensure this matches the correct file name
+import iphoneImage from './images/phone1.png'; // Ensure this matches the correct file name
+import iphoneVideo from './images/phone.mp4';
 import ctaButtonSvg from './images/ctaButton_responsive.svg'; // Import your SVG
 
 function App() {
@@ -10,22 +14,36 @@ function App() {
     <div className="App">
       <header className="hero">
         <div className="hero-content">
-          {/* Centered image */}
-          <img src={repeatTextImg} alt="Repeat Text" className="centered-image" />
+          {/* h2 above the game images */}
+          <h2>IT'S</h2>
+
+          {/* Grouped images */}
+          <div className="game-images">
+            <img src={game4} alt="Game 4" className="game-image game4" />
+            <img src={game3} alt="Game 3" className="game-image game3" />
+            <img src={game2} alt="Game 2" className="game-image game2" />
+            <img src={game1} alt="Game 1" className="game-image game1" />
+          </div>
+
+          {/* h1 below the game images */}
+          <h1>TIME</h1>
 
           {/* Button with image */}
           <button className="hero-button">
             <img src={ctaButtonSvg} alt="CTA Button" className="button-icon" />
           </button>
-          
+
           {/* Text below the button */}
           <p className="telegram-text">PLAY NOW ON TELEGRAM</p>
         </div>
       </header>
-      
-      {/* Bottom iPhone image */}
+
+       {/* Bottom iPhone video */}
       <div className="bottom-image-container">
-        <img src={iphoneImage} alt="iPhone" className="bottom-image" />
+        <video className="bottom-image" autoPlay loop muted>
+          <source src={iphoneVideo} type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
       </div>
     </div>
   );
